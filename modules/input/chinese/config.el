53fcc6b2ce460ab2ae43029d30fe3ee98ea095de
Levin Du <zslevin@gmail.com>
2022-11-22 17:19:58 +0800

    Pin input/chinese module

--- modules/input/chinese/config.el
+++ modules/input/chinese/config.el
@@ -1,7 +1,6 @@
 ;;; input/chinese/config.el -*- lexical-binding: t; -*-
 
 (use-package! pyim
-  :after-call after-find-file pre-command-hook
   :init
   (setq pyim-dcache-directory (concat doom-cache-dir "pyim/"))
   :config
